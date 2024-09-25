ceda9fcc7c2353723058117196b91b0e471773eb
Nabil Khalid <holanabilkhalid@gmail.com>
2024-09-26 00:04:54 +0800

    feat: add Express app with routing and middleware

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("node:path");
+const app = express();
+const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
+const messageRouter = require("./routes/messageRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
+app.use("/message", messageRouter);
+
+const PORT = 3000;
+app.listen(PORT);
