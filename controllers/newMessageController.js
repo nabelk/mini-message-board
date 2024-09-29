3584e9f39099e49857c873dfc4cef2f1855eb7f7
Nabil Khalid <holanabilkhalid@gmail.com>
2024-09-30 01:46:42 +0800

    refactor: add sanitization on the form subumit values

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,15 +1,22 @@
 const messages = require("../models/messages");
+const validator = require("validator");
 
 const showForm = (req, res) => {
   res.render("form");
 };
 
 const sendForm = (req, res) => {
+  const [sanitizedAuthor, sanitizedMessage] = [
+    validator.escape(req.body.author.trim()),
+    validator.escape(req.body.message.trim()),
+  ];
+
   messages.unshift({
-    text: req.body.message,
-    user: req.body.author,
+    text: sanitizedMessage,
+    user: sanitizedAuthor,
     added: new Date(),
   });
+  console.table(messages);
   res.redirect("/");
 };
 
